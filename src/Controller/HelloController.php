a782547a8f3e045ccdc4c210a9b98d99efe7ee2c
Michael Perigaud <mich.perigaud@gmail.com>
2021-05-28 16:03:09 +0200

    update 20210528

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,38 @@
+<?php
+/*
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/hello', name: 'hello')]
+    public function index(): Response
+    {
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+}
+
+<?php
+*/
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/plain", name="plain")
+     */
+    public function helloPlain(): Response
+    {
+        return new Response("Hello there", Response::HTTP_OK,
+            ['content-type' => 'text/plain']);
+    }
+}
